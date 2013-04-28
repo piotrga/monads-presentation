c4d0c621321bb9a0b8289652272c78e8c69cfadb
Piotr Gabryanczyk <piotrga@gmail.com>
2013-04-28 20:45:11 +0100

    02_numeric

---
+++ src/main/scala/monads/cassandra/livedemo/_02_Numeric.scala
@@ -0,0 +1,14 @@
+package x
+
+object _02_Numeric extends App{
+  //  Numeric
+  def multiply[A](vector1 : List[A], vector2 : List[A])(implicit num: Numeric[A]): A = (for{
+    e1 <- vector1
+    e2 <- vector2
+  } yield num.times(e1,e2)).sum
+
+
+  println ( multiply(List(1L, 2L),List(3L, 5L) ))
+  println( multiply(List(1.2, 2.3), List(3.4, 5.3) ))
+
+}
