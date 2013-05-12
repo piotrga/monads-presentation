02e55f069f915443cc52e04cbd521a02ac17fe8b
Piotr Gabryanczyk <piotrga@gmail.com>
2013-05-12 18:10:07 +0100

    moand slides

--- src/main/scala/livedemo/_03_monads/Monad.scala
+++ src/main/scala/livedemo/_03_monads/Monad.scala
@@ -13,11 +13,24 @@ object Monad {
     def >>[B](action: Monad[R, B]) = flatMap(_ => action)
     def flatMap[B](f: A => Monad[R, B]) : Monad[R,B] = (k: R) => f(self(k))(k)
     def map[B](f:A=>B) : Monad[R, B] = (r: R) => f(self(r))
-
   }
+
   type CassandraMonad[A] = Monad[Keyspace, A]
 
   def Read[T](id:String) : CassandraMonad[Option[T]] = null
   def Save[T](o:T) : CassandraMonad[Unit] = null
+
+}
+
+object CassandraMonad{
+
+  trait CassandraMonad[A]{ self =>
+    def apply(k: Keyspace): A
+    def unit[B](a:B) : CassandraMonad[B]
+    def flatMap[B](f: A => CassandraMonad[B]) : CassandraMonad[B]
+
+    def >>[B](action: CassandraMonad[B]) = flatMap(_ => action)
+    def map[B](f:A=>B) : CassandraMonad[B] = flatMap(a => unit(f(a)))
+  }
 
 }
--- src/main/scala/livedemo/_03_monads/_07_Collections.scala
+++ src/main/scala/livedemo/_03_monads/_07_Collections.scala
@@ -7,12 +7,13 @@ object _07_Collections extends App{
   List(1,2,3).flatMap(a => List.fill(a)(a*a) ) // List(1, 4, 4, 9, 9, 9)
 
   // Implicit conversion: Option -> Iterable
+
   def readBalance(id : String) : Option[Double] = id match {
     case "1" => Some(100)
     case "2" => Some(200)
     case _ => None
   }
 
-  List("1","2","3").flatMap(id => readBalance(id) ).sum // 300
+  List("1","2","3") flatMap(id => readBalance(id) ) sum; // 300
 
 }
